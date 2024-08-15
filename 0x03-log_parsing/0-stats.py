cda6f6a9431bb0fea415c9a886c8c4d84a813e39
Aliy Zainul Abideen Tahiru <78854433+AlhajiTarzain@users.noreply.github.com>
2024-08-15 17:39:33 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,57 +3,52 @@
 A script: Reads standard input line by line and computes metrics
 """
 
+
 def parseLogs():
     """
-    Reads logs from standard input and generates reports.
+    Reads logs from standard input and generates reports
     Reports:
-        * Prints log size after reading every 10 lines & at KeyboardInterrupt.
+        * Prints log size after reading every 10 lines & at KeyboardInterrupt
     Raises:
-        KeyboardInterrupt: Handles this exception to print the report.
+        KeyboardInterrupt (Exception): handles this exception and raises it
     """
     stdin = __import__('sys').stdin
     lineNumber = 0
     fileSize = 0
     statusCodes = {}
     codes = ('200', '301', '400', '401', '403', '404', '405', '500')
-    
     try:
         for line in stdin:
             lineNumber += 1
             line = line.split()
-            
             try:
-                # Extract file size and status code from the line
                 fileSize += int(line[-1])
                 if line[-2] in codes:
-                    statusCodes[line[-2]] = statusCodes.get(line[-2], 0) + 1
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
             except (IndexError, ValueError):
-                # Skip lines that don't match the expected format
                 pass
-            
             if lineNumber == 10:
                 report(fileSize, statusCodes)
                 lineNumber = 0
-        
-        # Final report after all lines are processed
         report(fileSize, statusCodes)
-    
-    except KeyboardInterrupt:
-        # Print the report on a KeyboardInterrupt (e.g., CTRL + C)
+    except KeyboardInterrupt as e:
         report(fileSize, statusCodes)
         raise
 
 
 def report(fileSize, statusCodes):
     """
-    Prints generated report to standard output.
+    Prints generated report to standard output
     Args:
-        fileSize (int): Total log size after every 10 successfully read lines.
-        statusCodes (dict): Dictionary of status codes and their counts.
+        fileSize (int): total log size after every 10 successfully read line
+        statusCodes (dict): dictionary of status codes and counts
     """
     print("File size: {}".format(fileSize))
-    for key in sorted(statusCodes.keys()):
-        print("{}: {}".format(key, statusCodes[key]))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
 
 if __name__ == '__main__':
